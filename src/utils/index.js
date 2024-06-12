cdc8a8a7dc8446e1a2af752c74ad67d13bdf7d22
jung <eugenejung04@gmail.com>
2024-06-12 01:12:44 -0400

    fix name typos

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,9 +5,9 @@ export const heroImg = hero;
 import hmv from "/assets/videos/hero.mp4";
 import smallmv from "/assets/videos/smallHero.mp4";
 import highlightFirstmv from "/assets/videos/highlight-first.mp4";
-import highlightSectmv from "/assets/videos/hightlight-third.mp4";
-import highlightThirdmv from "/assets/videos/hightlight-sec.mp4";
-import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
+import highlightSectmv from "/assets/videos/highlight-third.mp4";
+import highlightThirdmv from "/assets/videos/highlight-second.mp4";
+import highlightFourthmv from "/assets/videos/highlight-fourth.mp4";
 import exploremv from "/assets/videos/explore.mp4";
 import framemv from "/assets/videos/frame.mp4";
 
