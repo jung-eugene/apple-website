301ffc9db2058a5b19e02b5a1fd0c6249e30f652
jung <eugenejung04@gmail.com>
2024-06-12 22:20:42 -0400

    add phone model view by converting gltf into jsx

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,37 @@
-const ModelView = () => {
-  return <div>ModelView</div>;
+import { PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+import { Suspense } from "react";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2
+      } ? 'right-[-100%] : ''`}
+    >
+      {/* ambient light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <Suspense fallback={<div>Loading</div>}>
+        <IPhone />
+      </Suspense>
+    </View>
+  );
 };
 
 export default ModelView;
